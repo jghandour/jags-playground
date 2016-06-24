44d633a6190427e4bfbb008d44af478ff859c0ce
James Ghandour <jamesghandour@gmail.com>
2016-06-23 21:26:28 -0400

    Add a FT for confirming 404 works as expected

--- src/test/java/com/jagsits/web/MainFT.java
+++ src/test/java/com/jagsits/web/MainFT.java
@@ -1,11 +1,12 @@
 package com.jagsits.web;
 
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class MainFT extends BaseFT {
 
@@ -20,6 +21,16 @@ public class MainFT extends BaseFT {
     public void testCsrf() {
         Map response = getResultMap(doGet(MainController.MAPPING_CSRF));
         assertEquals(3, response.size());
+    }
+
+    @Test
+    public void test404() throws Exception {
+        try {
+            doGet("/INVALID_URL");
+            fail();
+        } catch (HttpClientErrorException hcee) {
+            assertEquals(HttpStatus.NOT_FOUND, hcee.getStatusCode());
+        }
     }
 
 }
--- src/test/java/com/jagsits/web/MockMvcMainControllerIT.java
+++ src/test/java/com/jagsits/web/MockMvcMainControllerIT.java
@@ -1,17 +1,13 @@
 package com.jagsits.web;
 
-import org.junit.Ignore;
 import org.junit.Test;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 
 public class MockMvcMainControllerIT extends BaseMockMvcSpringControllerIT {
 
@@ -21,13 +17,6 @@ public class MockMvcMainControllerIT extends BaseMockMvcSpringControllerIT {
         Map responseMap = getResultMap(mvcResult);
         assertNotNull(responseMap.get(MainController.RESPONSE_VERSION).toString());
         assertNotNull(responseMap.get(MainController.RESPONSE_TIMESTAMP).toString());
-    }
-
-    @Ignore
-    // FIXME: This is an issue with the default servlet vs. spring mvc resources
-    public void exceptionCase() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(get("/INVALID_URL").with(csrf().asHeader())).andReturn();
-        assertEquals(HttpStatus.NOT_FOUND.value(), mvcResult.getResponse().getStatus());
     }
 
 }
