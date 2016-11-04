8980d2d41334e22674a0aa6ecd53bd3de3b64bed
James Ghandour <jamesghandour@gmail.com>
2016-11-04 18:16:43 -0400

    convert it to a war file

--- src/main/java/com/jagsits/PlaygroundApplication.java
+++ src/main/java/com/jagsits/PlaygroundApplication.java
@@ -2,11 +2,18 @@ package com.jagsits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PlaygroundApplication {
+public class PlaygroundApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PlaygroundApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(PlaygroundApplication.class, args);
     }
 }
