e937ebdf04edf5b8c1938ce6e35bdf4bf2da4c8d
James Ghandour <jamesghandour@gmail.com>
2016-11-07 18:55:34 -0500

    - return ResponseEntity instead of map

--- src/main/java/com/jagsits/web/BaseController.java
+++ src/main/java/com/jagsits/web/BaseController.java
@@ -3,6 +3,7 @@ package com.jagsits.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
@@ -22,15 +23,16 @@ public class BaseController {
         return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, e.toString());
     }
 
-    Map buildResponse(Object object) {
+    ResponseEntity<Map> buildResponse(Object object) {
         return buildResponse(HttpStatus.OK, object);
     }
 
-    Map buildResponse(HttpStatus httpStatus, Object object) {
-        Map<String, Object> result = new HashMap<>();
-        result.put(RESPONSE_STATUS, httpStatus.value());
-        result.put(RESPONSE_RESULT, object);
-        return result;
+    ResponseEntity<Map> buildResponse(HttpStatus httpStatus, Object object) {
+        Map<String, Object> body = new HashMap<>();
+        body.put(RESPONSE_STATUS, httpStatus.value());
+        body.put(RESPONSE_RESULT, object);
+
+        return ResponseEntity.status(httpStatus).body(body);
     }
 
 }
